ef780e4e64ade53c4519291a2d7a6f0b5a16ce07
Josh <josh.zollweg@gmail.com>
2016-03-19 13:54:00 -0600

    Added Vector class

--- asp/objects.py
+++ asp/objects.py
@@ -24,6 +24,11 @@ class Point(object):
         ----------
         xyz : array_like
             Three-element array_like specifying coordinates in R3.
+
+        Returns
+        -------
+        Point
+            Instantiation of Point.
         """
         self._xyz = xyz
 
@@ -60,3 +65,38 @@ class Point(object):
         """
         self.xyz = xyz
         return self
+
+
+class Vector(Point):
+    """
+    Represents a vector in R3.
+
+    Attributes
+    ----------
+    xyz : array_like
+        Three-element array_like specifying coordinates in R3.
+    dir : array_like
+        Three-element array_like specifying vector direction.
+    """
+
+    def __init__(self, xyz, dir):
+        """Initializes a Vector object.
+
+        Parameters
+        ----------
+        xyz : array_like
+            Three-element array_like specifying coordinates in R3.
+        dir : array_like
+            Three-element array_like specifying vector direction.
+
+        Returns
+        -------
+        Vector
+            Instantiation of Vector.
+        """
+        self._dir = dir
+        super(Vector).__init__(xyz)
+
+    @property
+    def dir(self):
+        return self._dir
