d776dc7155a715138f639f6e4cf9d16bae0aeb5a
Josh <josh.zollweg@gmail.com>
2016-03-19 17:20:29 -0600

    Added math module and basic operators.

---
+++ asp/math.py
@@ -0,0 +1,86 @@
+import numpy as np
+from asp.objects import Point, Vector
+
+
+def add(points):
+    """Sequentially adds iterable of Points.
+    
+    Parameters
+    ----------
+    points : iter[Points]
+        Iterable of Point objects.
+
+    added : Point
+        Point with added xyz coordinates of all Points in input.
+    """
+    sum = points[0].xyz
+    for point in Points[1:]:
+        sum += point.xyz
+    return Point(sum)
+
+
+def mul(points):
+    """Sequentially multiplies iterable of Points.
+    
+    Parameters
+    ----------
+    points : iter[Points]
+        Iterable of Point objects.
+
+    Point
+        Point with multiplied xyz coordinates of all Points in input.
+    """
+    prod = points[0].xyz
+    for point in Points[1:]:
+        prod *= point.xyz
+    return Point(prod)
+
+
+def sub(points):
+    """Sequentially subtracts iterable of Points.
+    
+    Parameters
+    ----------
+    points : iter[Points]
+        Iterable of Point objects.
+
+    Point
+        Point with subtracted xyz coordinates of all Points in input.
+    """
+    diff = points[0].xyz
+    for point in Points[1:]:
+        diff -= point.xyz
+    return Point(diff)   
+
+
+def divides(points):
+    """Sequentially subtracts iterable of Points.
+    
+    Parameters
+    ----------
+    points : iter[Points]
+        Iterable of Point objects.
+
+    Point
+        Point with subtracted xyz coordinates of all Points in input.
+    """
+    quo = points[0].xyz
+    for point in Points[1:]:
+        quo /= point.xyz
+    return Point(quo)  
+
+
+def normal(plane):
+    """Computes the normal Vector of a Plane.
+    
+    Parameters
+    ----------
+    plane : Plane
+        Plane object to compute normal Vector for. 
+
+    Returns 
+    --------
+    normal : Vector
+        Normal Vector of parameter Plane.
+    """
+    v1 = Vector(
