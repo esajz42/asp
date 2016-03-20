c4f89d329df1d2cdc813a47565134cd8f440f3f9
Josh <josh.zollweg@gmail.com>
2016-03-20 11:47:05 -0600

    Added camera module and class

---
+++ asp/camera.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class camera(object):
+    """
+    Camera used to image a scene. 
+
+    Attributes
+    ----------
+    focal : float, [meters]
+        Camera system focal length. 
+    array_size : tuple, (2,), [pixels]
+        Number of pixels along x and y dimensions of detector.
+    pixel_size : tuple, (2,), [meters]
+        Size of each pixel along x and y dimensions.
+    """
+
+    def __init__(self, focal, array_size, pixel_size):
+        self.focal = focal
+        self.array_size = array_size
+        self.pixel_size = pixel_size
